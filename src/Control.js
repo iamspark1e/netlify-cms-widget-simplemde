c54f3680d4b69d483e6fa952b130951b1f13b17e
arctome <sparkinglemon@foxmail.com>
2023-03-08 03:08:29 +0800

    dev: add image gallery tool button

--- src/Control.js
+++ src/Control.js
@@ -2,13 +2,25 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { nanoid } from 'nanoid';
 
 export default class Control extends React.Component {
+  constructor(props) {
+    super(props)
+    let id = nanoid();
+    this.controlID = id;
+    this.state = {
+      mediaValue: null,
+    }
+  }
+
   static propTypes = {
     onChange: PropTypes.func.isRequired,
     forID: PropTypes.string,
     value: PropTypes.node,
     classNameWrapper: PropTypes.string.isRequired,
+    onOpenMediaLibrary: PropTypes.func.isRequired,
+    onAddAssets: PropTypes.func.isRequired,
   }
 
   static defaultProps = {
@@ -19,6 +31,37 @@ export default class Control extends React.Component {
     console.log(msg)
   }
 
+  handleCustomMedia = () => {
+    this.props.onOpenMediaLibrary({
+      controlID: this.controlID,
+      value: this.state.mediaValue
+    })
+  }
+
+  shouldComponentUpdate(nextProps) {
+    /**
+     * Always update if the value or getAsset changes.
+     */
+    if (this.props.value !== nextProps.value || this.props.getAsset !== nextProps.getAsset) {
+      return true;
+    }
+
+    /**
+     * If there is a media path for this control in the state object, and that
+     * path is different than the value in `nextProps`, update.
+     */
+    const mediaPath = nextProps.mediaPaths.get(this.controlID);
+    if (mediaPath && nextProps.value !== mediaPath) {
+      return true;
+    }
+
+    return false;
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log(nextProps.mediaPaths.get(this.controlID))
+  }
+
   render() {
     const {
       forID,
@@ -27,10 +70,31 @@ export default class Control extends React.Component {
       classNameWrapper,
     } = this.props;
 
+    function handleAppendImage() {
+      this.props.value += `![](/assets/test.png)`
+    }
+
     return (
-      <SimpleMDE id={forID} className={classNameWrapper} value={value || ''} onChange={(val) => {
-        onChange(val);
-      }} />
+      <div>
+        <button onClick={this.handleCustomMedia}>Open Media Gallery</button>
+        <SimpleMDE 
+        id={forID} 
+        className={classNameWrapper} 
+        value={value || ''} 
+        options={{
+          toolbar: [
+            {
+              name: "upload to media gallery",
+              action: handleAppendImage,
+              text: "Images",
+              title: "Open Media Gallery"
+            }
+          ]
+        }}
+        onChange={(val) => {
+          onChange(val);
+        }} />
+      </div>
     );
   }
 }
