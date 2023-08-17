4645dcbb1230e926811aabefada0893f74f0b6e1
spark1e <wenbo@sparkle.im>
2023-08-17 14:23:28 +0000

    stash: vite migrate progress

--- vite.config.js
+++ vite.config.js
@@ -1,43 +1,51 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 import { resolve } from 'path'
 
-const devConfig = {
-    plugins: [react()],
-    server: {
-        port: process.env.PORT || 30081,
-        host: '0.0.0.0'
+export default defineConfig(({ command, mode }) => {
+    if(mode !== 'production' || command === 'serve') {
+        // console.log(process.env.PORT)
+        return {
+            plugins: [react()],
+            server: {
+                port: process.env.PORT || 30081,
+                host: '0.0.0.0'
+            },
+            preview: {
+                port: process.env.PORT || 30081,
+                host: '0.0.0.0'
+            },
+            publicDir: 'public'
+        }
     }
-}
-const prodConfig = {
-    plugins: [react(), cssInjectedByJsPlugin()],
-    build: {
-        lib: {
-            // Could also be a dictionary or array of multiple entry points
-            entry: resolve(__dirname, 'src/index.jsx'),
-            name: 'SimpleMDEControl',
-            // the proper extensions will be added
-            fileName: 'main',
-            formats: ['iife'],
-        },
-        sourcemap: true,
-        define: {
-            'process.env.NODE_ENV': '"production"'
-        },
-        // rollupOptions: {
-        //     // make sure to externalize deps that shouldn't be bundled
-        //     // into your library
-        //     external: ['react'],
-        //     output: {
-        //         // Provide global variables to use in the UMD build
-        //         // for externalized deps
-        //         globals: {
-        //             react: 'React',
-        //         },
-        //     },
-        // },
+    return {
+        plugins: [react(), cssInjectedByJsPlugin()],
+        build: {
+            lib: {
+                // Could also be a dictionary or array of multiple entry points
+                entry: resolve(__dirname, 'src/index.jsx'),
+                name: 'SimpleMDEControl',
+                // the proper extensions will be added
+                fileName: 'main',
+                formats: ['umd'],
+            },
+            sourcemap: true,
+            define: {
+                'process.env.NODE_ENV': JSON.stringify(mode),
+            }
+            // rollupOptions: {
+            //     // make sure to externalize deps that shouldn't be bundled
+            //     // into your library
+            //     external: ['react'],
+            //     output: {
+            //         // Provide global variables to use in the UMD build
+            //         // for externalized deps
+            //         globals: {
+            //             react: 'React',
+            //         },
+            //     },
+            // },
+        }
     }
-}
-
-export default defineConfig(({ mode }) => mode === 'production' ? prodConfig : devConfig)
+})
